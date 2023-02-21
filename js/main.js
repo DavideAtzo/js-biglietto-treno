56d228ab9b46520f633a9417b59278338800609c
DavideAtzo <atzo9477@gmail.com>
2023-02-21 15:49:41 +0100

    javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+//l'utente potrà inserire gli input grazie al prompt
+const km = Number(prompt('inserire i km che vorresti fare'))
+const etaUtente = Number(prompt('inserire la tua eta'))
+console.log(km);
+console.log(etaUtente);
+
+
+// Calcolo biglietto
+const prezzoAlKm = 0.21;
+let prezzoBiglietto= km * prezzoAlKm;
+console.log(prezzoBiglietto);
+
+// variabili sconto del biglietto 
+const minorenne = 18;
+const over65 = 65;
+
+if(etaUtente < 18)
+prezzoBiglietto = prezzoBiglietto * 20 / 100;
+
+else if(etaUtente >= 65)
+prezzoBiglietto = prezzoBiglietto * 40 / 100;
+
+// Inerisco i dati negli id per la visualizzazione degli input
+document.getElementById('km').innerHTML = km;
+document.getElementById('eta-utente').innerHTML = etaUtente;
+document.getElementById('prezzo-biglietto').innerHTML = prezzoBiglietto.toFixed(2);
+
