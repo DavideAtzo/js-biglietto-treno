fd5880727d74265b1f29b51a610297a8b40db2cb
DavideAtzo <atzo9477@gmail.com>
2023-02-22 09:57:18 +0100

    ops ...fix calcolo

--- js/main.js
+++ js/main.js
@@ -17,10 +17,10 @@ const minorenne = 18;
 const over65 = 65;
 
 if(etaUtente < 18)
-prezzoBiglietto = prezzoBiglietto * 20 / 100;
+prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.2;
 
 else if(etaUtente >= 65)
-prezzoBiglietto = prezzoBiglietto * 40 / 100;
+prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.4;
 
 // Inerisco i dati negli id per la visualizzazione degli input
 document.getElementById('km').innerHTML = km;
